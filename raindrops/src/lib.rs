3625312c91fc3e27d32086875d0fb990b5270eab
Matt Boersma <matt@sprout.org>
2018-07-04 12:21:17 -0600

    [raindrops] all tests passing

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut drops = String::new();
+    if n % 3 == 0 {
+        drops.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        drops.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        drops.push_str("Plong");
+    }
+    if drops.is_empty() {
+        drops = n.to_string();
+    }
+    drops
 }
