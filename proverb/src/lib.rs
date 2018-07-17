6c576532450f130bd29b3e5b460f27d870ecf479
Matt Boersma <matt@sprout.org>
2018-07-17 13:49:17 -0600

    [proverb] all tests passing

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut s = String::new();
+
+    if list.len() > 0 {
+        for i in 0..list.len()-1 {
+            s += &format!("For want of a {} the {} was lost.\n", list[i], list[i+1]);
+        }
+        s += &format!("And all for the want of a {}.", list[0]);
+    }
+
+    s
 }
