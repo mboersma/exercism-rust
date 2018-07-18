f456bf3db8da8874a9608610e44836bf06bc30f8
Matt Boersma <matt@sprout.org>
2018-07-18 10:53:24 -0600

    [pythagorean-triplet] all tests passing

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn find() -> Option<u32> {
-    unimplemented!();
+    // find a pythagorean triplet where a + b + c = 1000
+   for a in 1..1000 {
+        for b in 1..1000 {
+            for c in 1..1000 {
+                if a + b + c > 1000 {
+                    break;
+                }
+                if a*a + b*b == c*c && a + b + c == 1000 {
+                    return Some(a*b*c);
+                }
+            }
+        }
+    }
+    None
 }
