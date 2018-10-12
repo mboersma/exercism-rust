e4c04fa6075d7fea5af66215d948c8d7c8054b23
Matt Boersma <matt@sprout.org>
2018-10-12 16:22:41 -0600

    [saddle-points] all tests passing

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,33 @@
+fn min_in_col(input: &[Vec<u64>], col: usize) -> u64 {
+    let mut res = u64::max_value();
+
+    for row in input.iter() {
+        for (col_idx, v) in row.iter().enumerate() {
+            if col_idx == col && v < &res {
+                res = v.clone()
+            }
+        }
+    }
+
+    res
+}
+
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut saddle_points = vec![];
+
+    for (row_idx, row) in input.iter().enumerate() {
+        // find the maximum value in this row
+        let max_in_row = *row.iter().max().unwrap_or(&0);
+
+        // find the minimum value in the max value's column
+        let col_idx = row.iter().position(|&v| v == max_in_row).unwrap_or(0);
+        let min_in_col = min_in_col(input, col_idx);
+
+        // if the values are equal, append the current location
+        if max_in_row == min_in_col {
+            saddle_points.push((row_idx, col_idx))
+        }
+    }
+
+    saddle_points
 }
