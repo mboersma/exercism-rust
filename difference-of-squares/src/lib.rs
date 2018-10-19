1d6eaa8ad45a43742f16a3bc1dfaeacbd9798ad2
Matt Boersma <matt@sprout.org>
2018-10-19 13:24:10 -0600

    [difference-of-squares] improved implementation

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,17 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let mut ret: u32 = 0;
-    for i in 1..n+1 {
-        ret += i
-    }
-    ret * ret
+    (1..n+1).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    let mut ret: u32 = 0;
-    for i in 1..n+1 {
-        ret += i * i
-    }
-    ret
+    (1..n+1).map(|x| x.pow(2)).sum::<u32>()
 }
 
 pub fn difference(n: u32) -> u32 {
