8e781c7397a966f568039354d237979e8b35212c
Matt Boersma <matt@sprout.org>
2018-10-23 16:08:04 -0600

    [sum-of-multiples] all tests passing

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,4 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    (1..limit).filter(|c| factors.iter().any(|f| c % f == 0))
+        .fold(0, |acc, c| acc+c)
 }
