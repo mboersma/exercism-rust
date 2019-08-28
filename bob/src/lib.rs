75af1af7e3f027f8fef77ae74333ecbf8b46eb00
Matt Boersma <matt@sprout.org>
2019-08-28 11:16:37 -0600

    [bob] updated tests and added comments

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,7 +1,10 @@
+//! Reply to conversation as Bob, the lackadaisical teenager.
+
+/// Return Bob's response to a statement, a question, or yelling.
 pub fn reply(message: &str) -> &str {
     let m = message.trim();
     // Does it have any letters and are they in ALL CAPS?
-    if m.chars().any(|c| c.is_alphabetic() ) && m == m.to_uppercase() {
+    if m.chars().any(|c| c.is_alphabetic()) && m == m.to_uppercase() {
         if m.ends_with("?") {
             return "Calm down, I know what I'm doing!";
         } else {
