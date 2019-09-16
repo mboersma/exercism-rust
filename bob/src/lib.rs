8b7769e59994467f67771c2f4c3bffa5e48f265f
Matt Boersma <matt@sprout.org>
2019-09-16 10:49:54 -0600

    [bob] use constants for output strings

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,19 +1,25 @@
 //! Reply to conversation as Bob, the lackadaisical teenager.
 
+const CALM_DOWN: &str = "Calm down, I know what I'm doing!";
+const WHOA_CHILL: &str = "Whoa, chill out!";
+const SURE: &str = "Sure.";
+const FINE: &str = "Fine. Be that way!";
+const WHATEVER: &str = "Whatever.";
+
 /// Return Bob's response to a statement, a question, or yelling.
 pub fn reply(message: &str) -> &str {
     let m = message.trim();
     // Does it have any letters and are they in ALL CAPS?
     if m.chars().any(|c| c.is_alphabetic()) && m == m.to_uppercase() {
         if m.ends_with("?") {
-            return "Calm down, I know what I'm doing!";
+            return CALM_DOWN;
         } else {
-            return "Whoa, chill out!";
+            return WHOA_CHILL;
         }
     } else if m.ends_with("?") {
-        return "Sure.";
+        return SURE;
     } else if m == "" {
-        return "Fine. Be that way!";
+        return FINE;
     }
-    "Whatever."
+    WHATEVER
 }
