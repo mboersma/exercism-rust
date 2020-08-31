fd826d171251170a80de365783cffab665e9cce7
Matt Boersma <matt@sprout.org>
2020-08-31 16:16:35 -0600

    [prime-factors] all tests passing

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,21 @@
+/// Computes the prime factors of a given natural number.
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(prime_factors::factors(147), vec![3, 7, 7]);
+/// ```
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut factors = Vec::new();
+    let mut n = n;
+    let mut d = 2;
+    while d <= n {
+        if n % d == 0 {
+            factors.push(d);
+            n /= d;
+        } else {
+            d += 1;
+        }
+    }
+    factors
 }
