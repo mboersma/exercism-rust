d114a7ca7ae967724b2a14289d1a28f25a641a8e
Matt Boersma <matt@sprout.org>
2020-08-31 16:54:43 -0600

    [armstrong-numbers] all tests passing

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,17 @@
+/// Checks if a number is an "Armstrong number": a number that is the sum of its
+/// own digits each raised to the power of the number of digits.
+///
+/// # Examples
+///
+/// ```
+/// assert!(armstrong_numbers::is_armstrong_number(153));
+/// assert!(!armstrong_numbers::is_armstrong_number(154));
+/// ```
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let s = num.to_string();
+    let exp = s.len() as u32;
+    num == s.chars()
+        .filter_map(|n| n.to_digit(10))
+        .map(|n| (n as u32).pow(exp))
+        .sum()
 }
