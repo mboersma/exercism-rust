cf824bbcda6f82f70342c60a44816c174d813d64
Matt Boersma <matt@sprout.org>
2020-09-01 09:26:54 -0600

    [proverb] update to current exercism version

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,9 +1,9 @@
-pub fn build_proverb(list: Vec<&str>) -> String {
+pub fn build_proverb(list: &[&str]) -> String {
     let mut s = String::new();
 
-    if list.len() > 0 {
-        for i in 0..list.len()-1 {
-            s += &format!("For want of a {} the {} was lost.\n", list[i], list[i+1]);
+    if !list.is_empty() {
+        for i in 0..list.len() - 1 {
+            s += &format!("For want of a {} the {} was lost.\n", list[i], list[i + 1]);
         }
         s += &format!("And all for the want of a {}.", list[0]);
     }
