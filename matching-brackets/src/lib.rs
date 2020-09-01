26c618120c4c45dc629ad51b4dc6e2da0b6c7dad
Matt Boersma <matt@sprout.org>
2020-09-01 16:17:28 -0600

    [matching-brackets] all tests passing

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,45 @@
+/// Verify that pairs of brackets, braces, and parentheses match and nest
+/// properly in a given string.
+///
+/// # Examples
+///
+/// ```
+/// let s = "[if(contains(split('{canadacentral},{centralus}',','),variables('location')),3,if(equals('{centraluseuap}',variables('location')),1,2))]";
+/// assert!(matching_brackets::brackets_are_balanced(s));
+/// ```
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut stack = String::new();
+
+    for c in string.chars() {
+        match c {
+            '[' | '{' | '(' => stack.push(c),
+            ']' => {
+                if !top(&mut stack, '[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if !top(&mut stack, '{') {
+                    return false;
+                }
+            }
+            ')' => {
+                if !top(&mut stack, '(') {
+                    return false;
+                }
+            }
+            _ => continue,
+        }
+    }
+
+    stack.is_empty()
+}
+
+/// Pops the top off the stack and returns whether it equals the given char.
+fn top(stack: &mut String, c: char) -> bool {
+    let p = stack.pop();
+    match p {
+        Some(p) => p == c,
+        None => false,
+    }
 }
