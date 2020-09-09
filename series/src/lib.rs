4fe34faed3b9176acd147a64edab08abae310f30
Matt Boersma <matt@sprout.org>
2020-09-09 17:08:17 -0600

    [series] all tests passing

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,7 +1,13 @@
+//! series finds contiguous substrings in a string.
+
+// Returns a vector of substrings with the given length.
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    let mut subs = vec![];
+    if len <= digits.len() {
+        let end = digits.len() - len;
+        for i in 0..=end {
+            subs.push(digits[i..i + len].to_string());
+        }
+    }
+    subs
 }
