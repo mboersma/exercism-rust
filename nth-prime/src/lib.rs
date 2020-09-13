9ced1948d387d001edc20b4e3f59951c41029e91
Matt Boersma <matt@sprout.org>
2020-09-13 11:43:49 -0600

    [nth-prime] refactored and all tests passing

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,50 +1,15 @@
-struct PrimeCandidates {
-    index: u32,
+//! Calculates the nth prime starting from zero.
+
+// All credit for this solution goes to plippe at exercism. After experimentation,
+// I threw out my old PrimeCandidates approach for something more functional and
+// came up with a horrible version of this, but then I saw his solution, and...bravo!
+
+// Returns true if n is a prime number.
+pub fn is_prime(n: u32) -> bool {
+    !(2..n - 1).any(|i| n % i == 0)
 }
 
-impl PrimeCandidates {
-    fn new() -> PrimeCandidates {
-        PrimeCandidates { index: 0 }
-    }
-}
-
-impl Iterator for PrimeCandidates {
-    // iterate over the sequence 2, 3, 5, 7, 9, 11...
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index == 2 {
-            self.index += 1;
-        } else {
-            self.index += 2;
-        }
-        Some(self.index)
-    }
-}
-
-pub fn nth(n: u32) -> Option<u32> {
-    if n == 0 {
-        return None;
-    }
-
-    let mut v: Vec<u32> = Vec::new();
-
-    for i in PrimeCandidates::new() {
-        for j in PrimeCandidates::new() {
-            if j >= i {
-                // prime number, since no smaller divisor was ruled out
-                v.push(i);
-                if v.len() as u32 == n {
-                    return Some(i);
-                }
-                break;
-            }
-            if i % j == 0 {
-                // not a prime since it's even divisble by a smaller number
-                break;
-            }
-        }
-    }
-
-    None
+// Returns the nth prime starting from zero.
+pub fn nth(n: u32) -> u32 {
+    (2..).filter(|c| is_prime(*c)).nth(n as usize).unwrap()
 }
