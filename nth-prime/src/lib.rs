16bcd57c07ffbdd5cebd11f52b8e7054bd914eaa
Matt Boersma <matt@sprout.org>
2020-09-13 11:53:07 -0600

    [nth-prime] added comments and minor optimization

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,15 +1,18 @@
-//! Calculates the nth prime starting from zero.
+//! Given a number n, determine what the nth prime is.
+/// All credit for this solution goes to plippe at exercism!
 
-// All credit for this solution goes to plippe at exercism. After experimentation,
-// I threw out my old PrimeCandidates approach for something more functional and
-// came up with a horrible version of this, but then I saw his solution, and...bravo!
-
-// Returns true if n is a prime number.
-pub fn is_prime(n: u32) -> bool {
-    !(2..n - 1).any(|i| n % i == 0)
-}
-
-// Returns the nth prime starting from zero.
+/// Returns the nth prime in the series 2, 3, 5, 7, 11, 13, ...
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(nth_prime::nth(5), 13);
+/// ```
 pub fn nth(n: u32) -> u32 {
     (2..).filter(|c| is_prime(*c)).nth(n as usize).unwrap()
+}
+
+/// Returns true if n is a prime number.
+pub fn is_prime(n: u32) -> bool {
+    !(2..n / 2 + 1).any(|i| n % i == 0)
 }
