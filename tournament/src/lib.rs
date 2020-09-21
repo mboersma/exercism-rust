dab3b88620611f1679b0ba8b4afd3ad131abb75e
Matt Boersma <matt@sprout.org>
2020-09-21 17:44:16 -0600

    [tournament] improvements

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -1,27 +1,23 @@
+//! Tallies up and summarizes the results of a football competition.
+
 use std::collections::HashMap;
 
-struct Stats {
-    name: String,
+#[derive(Clone, Copy)]
+struct Team<'a> {
+    name: &'a str,
     wins: u32,
     losses: u32,
     draws: u32,
 }
 
-impl Stats {
-    pub fn new(name: &str) -> Self {
+impl<'a> Team<'a> {
+    pub fn new(name: &'a str) -> Self {
         Self {
-            name: name.to_string(),
+            name: name,
             wins: 0,
             losses: 0,
             draws: 0,
         }
-    }
-
-    pub fn matches_played(&self) -> u32 {
-        self.wins + self.losses + self.draws
-    }
-    pub fn points(&self) -> u32 {
-        self.wins * 3 + self.draws
     }
 
     pub fn add_win(&mut self) {
@@ -35,43 +31,82 @@ impl Stats {
     pub fn add_draw(&mut self) {
         self.draws += 1;
     }
+
+    pub fn name(&self) -> &str {
+        self.name
+    }
+
+    pub fn matches_played(&self) -> u32 {
+        self.wins + self.losses + self.draws
+    }
+
+    pub fn wins(&self) -> u32 {
+        self.wins
+    }
+
+    pub fn draws(&self) -> u32 {
+        self.draws
+    }
+
+    pub fn losses(&self) -> u32 {
+        self.losses
+    }
+
+    pub fn points(&self) -> u32 {
+        self.wins * 3 + self.draws
+    }
 }
 
 /// Tallies up and summarizes the results of a football competition.
 pub fn tally(match_results: &str) -> String {
-    let mut competition: HashMap<&str, Stats> = HashMap::new();
+    let mut competition: HashMap<&str, Team> = HashMap::new();
+
     for line in match_results.lines() {
         if line == "" || line.starts_with("#") {
             continue;
         }
+
         let parts: Vec<&str> = line.split(";").collect();
         assert_eq!(parts.len(), 3);
 
         if !competition.contains_key(parts[0]) {
-            competition.insert(parts[0], Stats::new(parts[0]));
+            competition.insert(parts[0], Team::new(parts[0]));
         }
         if !competition.contains_key(parts[1]) {
-            competition.insert(parts[1], Stats::new(parts[1]));
+            competition.insert(parts[1], Team::new(parts[1]));
         }
-        let mut team1 = competition.get(parts[0]).unwrap();
-        let mut team2 = competition.get(parts[1]).unwrap();
 
         let result = parts[2];
+        let team1 = competition.get_mut(parts[0]).unwrap();
         match result {
-            "win" => {
-                team1.add_win();
-                team2.add_loss();
-            }
-            "loss" => {
-                team1.add_loss();
-                team2.add_win();
-            }
-            "draw" => {
-                team1.add_draw();
-                team2.add_draw();
-            }
+            "win" => team1.add_win(),
+            "loss" => team1.add_loss(),
+            "draw" => team1.add_draw(),
+            _ => (),
+        }
+
+        let team2 = competition.get_mut(parts[1]).unwrap();
+        match result {
+            "win" => team2.add_loss(),
+            "loss" => team2.add_win(),
+            "draw" => team2.add_draw(),
             _ => (),
         }
     }
-    "Team                           | MP |  W |  D |  L |  P".to_string()
+
+    let mut s = String::from("Team                           | MP |  W |  D |  L |  P");
+    // TODO: sort order!
+    competition.retain(|_k, v| {
+        s.push_str(&format!(
+            "\n{:<30} | {:>2} | {:>2} | {:>2} | {:>2} | {:>2}",
+            v.name(),
+            v.matches_played(),
+            v.wins(),
+            v.draws(),
+            v.losses(),
+            v.points()
+        ));
+        true
+    });
+    s
 }
