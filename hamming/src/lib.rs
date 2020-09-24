10ab06feb2eb00bdc4259ce0cf11f817680635c3
Matt Boersma <matt@sprout.org>
2020-09-24 16:01:52 -0600

    [hamming] improved implementation

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,14 +1,10 @@
+use std::cmp::Ordering;
+
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    if s1.len() != s2.len() {
-        return None;
+    match s1.len().cmp(&s2.len()) {
+        Ordering::Equal => Some(s1.chars().zip(s2.chars()).filter(|(a, b)| a != b).count()),
+        _ => None,
     }
-    let mut dist: usize = 0;
-    for (a, b) in s1.chars().zip(s2.chars()) {
-        if a != b {
-            dist += 1;
-        }
-    }
-    Some(dist)
 }
