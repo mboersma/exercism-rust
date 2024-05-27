62c2beb4cbb570de1a5dba33ae1a0a42ee9f7d9b
Matt Boersma <matt@sprout.org>
2024-05-27 12:34:49 -0600

    Some tests passing

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -6,7 +6,7 @@ pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &'a[&'a str]) -> HashS
     // for each word in the candidates,
     for candidate in possible_anagrams {
         // if it's an anagram of the target word,
-        if target.eq_ignore_ascii_case(char_sort(candidate)) {
+        if target.eq_ignore_ascii_case(&char_sort(candidate)) {
             // add it to the list of matches
            anagrams.insert(*candidate);
         }
@@ -14,12 +14,12 @@ pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &'a[&'a str]) -> HashS
     anagrams
 }
 
-pub fn char_sort<'a>(word: &'a str) -> &'a str {
+pub fn char_sort<'a>(word: &'a str) -> String {
     let mut l: Vec<char> = word.chars().collect();
     l.sort_unstable();
     // l.into_iter().collect::<Vec<_>>().as_str()
-    let s: String = l.into_iter().collect();
+    l.into_iter().collect::<String>().to_lowercase()
     // s.as_str()
-    let j: &'a str = s.as_str();
-    j
+    // let j: &'a str = s.as_str();
+    // j
 }
