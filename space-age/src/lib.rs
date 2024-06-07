f90c581df227352744439f3ebba77d71d4601735
Matt Boersma <matt@sprout.org>
2024-06-07 15:02:06 -0600

    [space-age]: all tests passing

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -1,12 +1,17 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
+// space-age: Given an age in seconds, calculate how old
+// someone would be on a planet in our Solar System.
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    earth_years: f64,
+}
+
+const EARTH_YEAR: f64 = 60.0 * 60.0 * 24.0 * 365.25; // sidereal year is 365.256 days
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+        Self {
+            earth_years: (s as f64 / EARTH_YEAR),
+        }
     }
 }
 
@@ -25,11 +30,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 0.2408467
+    }
+}
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 0.61519726
+    }
+}
+impl Planet for Earth {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 1.0
+    }
+}
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 11.862615
+    }
+}
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 29.447498
+    }
+}
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 84.016846
+    }
+}
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 164.79132
+    }
+}
