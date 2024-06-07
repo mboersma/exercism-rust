5555a2f344d17154c73dd07b225fc4888b6444c0
Matt Boersma <matt@sprout.org>
2024-06-07 15:29:59 -0600

    [eliuds-eggs] simplify to one-liner

--- eliuds-eggs/src/lib.rs
+++ eliuds-eggs/src/lib.rs
@@ -1,4 +1,6 @@
 pub fn egg_count(display_value: u32) -> usize {
-    let binary_str = format!("{:b}", display_value);
-    binary_str.chars().filter(|&c| c == '1').count()
+    format!("{display_value:b}")
+        .chars()
+        .filter(|&c| c == '1')
+        .count()
 }
