7f9b2f825b51ab353088e2347455f35f26c559f8
Matt Boersma <matt@sprout.org>
2024-09-18 14:57:37 -0600

    [luhn] all tests passing

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,23 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    code.chars().all(|c| c.is_digit(10) || c.is_whitespace())
+        && code.chars().filter(|c| c.is_digit(10)).count() > 1
+        && code
+            .chars()
+            .filter(|c| c.is_digit(10))
+            .rev()
+            .enumerate()
+            .map(|(i, c)| {
+                let mut n = c.to_digit(10).unwrap();
+                if i % 2 == 1 {
+                    n *= 2;
+                    if n > 9 {
+                        n -= 9;
+                    }
+                }
+                n
+            })
+            .sum::<u32>()
+            % 10
+            == 0
 }
